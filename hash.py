86ffee0cefa6c51521d983589c2cbb3afd71ac91
Anna Tsolakou <tsolakou.a@gmail.com>
2018-01-22 17:37:49 +0100

    Update hash.py

--- hash.py
+++ hash.py
@@ -2,7 +2,7 @@ import hashlib
 import time
 import re
 
-#validate user input
+# validate user input
 while (True):
 	user_input = input("Type a password | len >= 6 & contains numbers: ")
 	if len(user_input) < 6:
@@ -14,25 +14,25 @@ while (True):
 
 encoded_input = user_input.encode('utf-8')
 
-#SHA-1
+# SHA-1
 start = time.time()
 hash_sha1 = hashlib.sha1(encoded_input)
 end = time.time()
 print ("SHA-1", hash_sha1.hexdigest(), "Time Elapsed: ", end - start) 
 
-#SHA-256
+# SHA-256
 start = time.time()
 hash_sha256 = hashlib.sha256(encoded_input)
 end = time.time()
 print ("SHA-256", hash_sha256.hexdigest(), "Time Elapsed: ", end - start) 
 
-#SHA-512
+# SHA-512
 start = time.time()
 hash_sha512 = hashlib.sha512(encoded_input)
 end = time.time()
 print ("SHA-512", hash_sha512.hexdigest(), "Time Elapsed: ", end - start) 
 
-#MD5
+# MD5
 start = time.time()
 hash_md5 = hashlib.md5(encoded_input)
 end = time.time()
